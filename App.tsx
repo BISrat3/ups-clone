dd9e0f8219d2e6e3afdc6ba243a7534e069bcfdc
bisrat <bisrat.amtataw@gmail.com>
2023-02-16 21:30:53 -0500

    tailwind

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {TailwindProvider} from 'tailwind-rn';
+import utilities from './tailwind.json';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" /> */}
+    // gts-ignore
+    <TailwindProvider utilities={utilities}>
+     <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
+   </TailwindProvider>
+    
   );
 }
 
