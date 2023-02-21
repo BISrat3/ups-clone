795cffc27ab908237f30127edb1e9131811b3a28
bisrat <bisrat.amtataw@gmail.com>
2023-02-20 22:08:50 -0500

    page update

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
+import { useTailwind } from 'tailwind-rn/dist';
 import React from 'react'
 
 export default function CustomersScreen() {
+    const tw = useTailwind(); 
   return (
-    <View>
-      <Text>CustomersScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text style={tw("")}>CustomersScreen</Text>
+    </SafeAreaView>
   )
 }
