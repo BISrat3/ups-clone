116c97c6cfc625aa46bf730aa802bc7094ed49ff
bisrat <bisrat.amtataw@gmail.com>
2023-02-24 22:17:55 -0500

    rootnavigator

--- navigator/RootNavigator.tsx
+++ navigator/RootNavigator.tsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
+const Stack = createNativeStackNavigator()
 const RootNavigator = () => {
+
   return (
     <View>
       <Text>RootNavigator</Text>
