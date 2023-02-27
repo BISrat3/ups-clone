93e7bafc2039e147688ca5b8cead7e7d454b3894
bisrat <bisrat.amtataw@gmail.com>
2023-02-26 19:41:50 -0500

    tabnavigator

--- navigator/RootNavigator.tsx
+++ navigator/RootNavigator.tsx
@@ -2,13 +2,15 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-const Stack = createNativeStackNavigator()
+const ReactStack = createNativeStackNavigator()
 const RootNavigator = () => {
 
   return (
-    <View>
-      <Text>RootNavigator</Text>
-    </View>
+    <ReactStack.Navigator>
+      <ReactStack.Group>
+        
+      </ReactStack.Group>
+    </ReactStack.Navigator>
   )
 }
 
