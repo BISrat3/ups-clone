24aaea10808d7ace705b38a29cf190ecc186f40b
bisrat <bisrat.amtataw@gmail.com>
2023-02-28 22:33:09 -0500

    rootnavigator

--- navigator/RootNavigator.tsx
+++ navigator/RootNavigator.tsx
@@ -3,6 +3,14 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import TabNavigator from './TabNavigator'
 
+export type RootStackParamList = {
+  Main: undefined;
+  MyModal : {
+    userId : string;
+    name: string;
+  }
+}
+
 const RootStack = createNativeStackNavigator()
 const RootNavigator = () => {
 
