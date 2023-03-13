68c2101ec3c2d11018bc764f4399980fba60ee8a
bisrat <bisrat.amtataw@gmail.com>
2023-03-12 21:58:03 -0400

    typing

--- hooks/useCustomerOrders.tsx
+++ hooks/useCustomerOrders.tsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/client'
 import { GET_ORDERS } from '../graphql/queries'
-import { SortOrder } from 'mongoose'
 
 const useCustomerOrders = () => {
     const {loading, error, data} = useQuery(GET_ORDERS)
     const [Orders, setOrders] = useState <Order[]>([])
+
+    useEffect(() =>{
+        if(!data)
+            return;
+//  create order array and access the order 
+//  transforming the data that we would like to use in
+        const orders: Order [] = data.getOrders.map(({value}: OrderResponse)=> ({
+            carrier: value.carrier,
+            createdAt: value.createdAt,
+            shippingCost: value.shippingCost,
+            trackingId: value.trackingId,
+            trackingItems: value.trackingItems,
+            Address: value.Address,
+            City: value.city,
+            Lat: value.Lat,
+            Lng: value.img,
+        }))
+    }, [data])
+
   return (
-    <div>useCustomerOrders</div>
+    <div></div>
   )
 }
 
