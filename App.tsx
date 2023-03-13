c8f820c453015972b7d69c517fef28939ad9bf21
bisrat <bisrat.amtataw@gmail.com>
2023-03-12 21:25:31 -0400

    graphql

--- App.tsx
+++ App.tsx
@@ -5,24 +5,31 @@ import {TailwindProvider} from 'tailwind-rn';
 import RootNavigator from './navigator/RootNavigator';
 import CustomersScreen from './screens/CustomersScreen';
 import utilities from './tailwind.json';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://dashboard.stepzen.com/explorer',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     //@ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
-    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ graphql/queries.ts
@@ -0,0 +1,41 @@
+import {gql} from "@apollo/client"
+
+export const GET_CUSTOMERS = gql`
+    query GetCustomers {
+        GetCustomers {
+            value {
+                emailname
+            }
+            name
+        }
+    }
+`
+
+export const GET_ORDERS = gql`
+    query getTrackingItems {
+        getOrders {
+            value {
+                Address
+                City
+                Lat
+                Lng
+                carrier
+                createAt
+                shippingCost
+                trackingId
+                trackingItem {
+                    customer {
+                    email
+                    name
+                    }
+                    items {
+                    item_id
+                    name
+                    price
+                    quantity
+                    }
+                }
+            }
+        }
+    }
+`
