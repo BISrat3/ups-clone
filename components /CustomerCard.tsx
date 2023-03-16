8c80fdbd2691e34fb837af26974a16cd957d415e
bisrat <bisrat.amtataw@gmail.com>
2023-03-16 16:56:03 -0400

    Customer Card

--- components /CustomerCard.tsx
+++ components /CustomerCard.tsx
@@ -22,19 +22,23 @@ const CustomerCard = ({userId, name, email}: Props) => {
       <Card containerStyle={tw("p-5 rounded-lg")}>
         <View>
           <View>
-            <Text style={tw("text-2xl font-bold")}>{name}</Text>
-            <Text style={[tw("text-sm"),{color:"59C1CC"}]}>ID: {userId}</Text>
-          </View>
-          
-          <View>
-            <Text>{loading ? "Loading ...": `${orders.length} x`}</Text>
-            <Icon
-              style= {tw("mb-5 ml-auto")}
-              name="box"
-              type="entypo"
-              color="#59C1CC"
-              size={50}
-            />
+            <View style={tw("flex-row justifying-between")}>
+              <Text style={tw("text-2xl font-bold")}>{name}</Text>
+              <Text style={[tw("text-sm"),{color:"59C1CC"}]}>ID: {userId}</Text>
+            </View>
+            
+            <View style={tw("flex-row items-center justify-end")}>
+              <Text style={{color:"59C1CC"}}>
+                {loading ? "Loading ...": `${orders.length} x`}
+              </Text>
+              <Icon
+                style= {tw("mb-5 ml-auto")}
+                name="box"
+                type="entypo"
+                color="#59C1CC"
+                size={50}
+              />
+            </View>
           </View>
         </View>
         <Card.Divider/>
