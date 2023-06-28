32dd5e4ce5c6fb7221535c95c3dfeab4f9006598
bisrat <bisrat.amtataw@gmail.com>
2023-06-27 22:43:58 -0400

    update app

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -2,29 +2,43 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Icon } from '@rneui/base'
 import { useTailwind } from 'tailwind-rn/dist'
-import { useNavigation } from '@react-navigation/native'
-import { CompositeNavigationProp } from '@react-navigation/native'
+import { RouteProp, useNavigation } from '@react-navigation/native'
+import { CompositeNavigationProp, useRoute,  } from '@react-navigation/native'
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
 import { RootStackParamList } from '../navigator/RootNavigator'
 import { TabStackParamList } from '../navigator/TabNavigator'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import useCustomerOrders from '../hooks/useCustomerOrders'
 
 // when we go to the second screen layer we need to have a composition layer type
 
-type ModalScreenNavigationProps = CompositeNavigationProp<
+type ModalScreenNavigationProp = CompositeNavigationProp<
 BottomTabNavigationProp<TabStackParamList>,
 NativeStackNavigationProp<RootStackParamList, "MyModal">
 >
 
+type ModalScreenRouteProp = RouteProp<RootStackParamList, "MyModal">
+
 const ModalScreen = () => {
   const tw= useTailwind()
-  const navigation = useNavigation()
+  const navigation = useNavigation<ModalScreenNavigationProp>()
+  const {
+    params: {name, userId},
+    } = useRoute<ModalScreenRouteProp>()
+  const {loading, error, orders} = useCustomerOrders(userId)
+
   return (
     <View>
       {/* z means layered to the top */}
-      <TouchableOpacity style={tw('absolute right-5 top-5 z-10')}>
+      <TouchableOpacity 
+        onPress={navigation.goBack} 
+        style={tw('absolute right-5 top-5 z-10')}>
         <Icon name='closecircle' type='antdesign' />
       </TouchableOpacity>
+      <View>
+        <Text>{name}</Text>
+        <Text>deliveries</Text>
+      </View>
     </View>
   )
 }
